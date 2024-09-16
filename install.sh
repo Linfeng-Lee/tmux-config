5148b34cce356bd352106d459c802ab6ae2dc062
lianshan <akira.404@foxmail.com>
2024-09-16 15:06:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 echo "Link tmux.conf"
-sudo ln -s -f $HOME/tmux-config/tmux.conf $HOME/.tmux.conf
+sudo ln -s -f ~/tmux-config/tmux.conf ~/.tmux.conf
 
 echo "Link tmux.conf.local"
-sudo ln -s -f $HOME/tmux-config/tmux.conf.local $HOME/.tmux.conf.local
+sudo ln -s -f ~/tmux-config/tmux.conf.local ~/.tmux.conf.local
+
+# Append aliases to .bashrc
+echo '# Tmux'
+echo 'alias tns="tmux new -s"' >> ~/.bashrc
+echo 'alias tl="tmux ls"' >> ~/.bashrc
+echo 'alias ta="tmux attach -t"' >> ~/.bashrc
+echo 'alias tk="tmux kill-session -t"' >> ~/.bashrc
+
+# Notify the user to reload .bashrc
+echo "Aliases added to .bashrc. Please run 'source ~/.bashrc' to apply changes."
